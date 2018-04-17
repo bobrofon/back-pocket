55147ef099b547d8ea76eaaa57f09b615505849f
bobrofon <bobrofon@gmail.com>
2018-04-17 22:16:15 +0700

    add storage impl

---
+++ internal/app/back-pocket/storage/proxy.go
@@ -0,0 +1,69 @@
+package storage
+
+import (
+	"net/http"
+	"github.com/bobrofon/back-pocket/internal/app/back-pocket/config"
+	"github.com/pkg/errors"
+	"net/url"
+	"io/ioutil"
+	"sync"
+)
+
+type ProxyStorage struct {
+	client   *http.Client
+	cache    map[string]string
+	selfURL  *url.URL
+	rwMutex  *sync.RWMutex
+}
+
+func NewProxyStorage(conf *config.BackPocketConf) *ProxyStorage {
+	transport := &http.Transport{Proxy:http.ProxyURL(conf.HTTPProxy)}
+	client := &http.Client{Transport:transport}
+	selfURL := &url.URL{Scheme:"http", Host:conf.ListenAddr}
+	rwMutex := &sync.RWMutex{}
+	return &ProxyStorage{client:client, selfURL:selfURL, rwMutex:rwMutex}
+}
+
+func (s *ProxyStorage) Put(key string, value string) error {
+	s.setCache(key, value)
+	_, err := s.Get(key)
+	if err != nil {
+		return errors.Wrap(err, "cannot get key via proxy")
+	}
+	return nil
+}
+
+func (s *ProxyStorage) Get(key string) (string, error) {
+	getURL := *s.selfURL
+	getURL.Path = key
+	req, err := http.NewRequest(http.MethodGet, getURL.RequestURI(), nil)
+	if err != nil {
+		return "", errors.Wrap(err, "cannot construct get request")
+	}
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return "", errors.Wrap(err, "cannot execute get request")
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.Errorf("unexpected proxy response status: %s", resp.Status)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "cannot read response body")
+	}
+	value := string(body)
+	s.setCache(key, value)
+	return value, nil
+}
+
+func (s *ProxyStorage) getCache(key string) string {
+	s.rwMutex.RLock()
+	defer s.rwMutex.RUnlock()
+	return s.cache[key]
+}
+
+func (s *ProxyStorage) setCache(key string, value string) {
+	s.rwMutex.Lock()
+	defer s.rwMutex.Unlock()
+	s.cache[key] = value
+}
